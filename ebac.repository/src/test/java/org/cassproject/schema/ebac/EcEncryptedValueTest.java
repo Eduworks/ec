383893dc43d4d2ac0a881445802bbe08183526bc
Devlin Junker <devlin.junker@eduworks.com>
2016-03-31 18:34:32 +0000

    Updated Tests, also added a repository save/delete test for public and owned objects

--- ebac.repository/src/test/java/org/cassproject/schema/ebac/EcEncryptedValueTest.java
+++ ebac.repository/src/test/java/org/cassproject/schema/ebac/EcEncryptedValueTest.java
@@ -237,7 +237,7 @@ public class EcEncryptedValueTest
 				.fromPem("-----BEGIN RSA PRIVATE KEY-----MIIEpAIBAAKCAQEAl4oeWRsroJqAR8UXkmkhDHMcRaDM6a9wsFWNkokyugMevv5TtwHVrm9HMJqVlXn1U1AiVS1rEYB7Iavi2IE7e8WV6GkLrGTrce+YcqhrwF3g104ZEuIC2aFCcLrxvXbSqmmuddS3lRMEwqdWkYIS7MKRSKUBpbydl8wScYxjzUSMNzQrvgUpNc//N7GkpEFXYLjYmVWhYdUp578nw/K9StRqTfku8U+Ub86U0mHXowms4spV8IoOlV53OLpOL+8QwcQYZ+koWL4B0rhz6cvelvBAFygx94t8IzZtRiBniaTF+1nBjyj4+R8PdprxFNnM+S2IQBqUdBKJ3oDoKMqdAwIDAQABAoIBAC3BWmB1P7sCa3FAJVnjvELSDttHLhfxDQlxC4oPOu3HO2VXzVcYirhciRY31qqHZHd/Xp5xVD64mHUWPSw5+QfqJNVDwm6PGjgQq+sSx1YSAm1/+zokW8/yTOlUyOD4G6uwtSiGzdeJIorTk+PjbmtmZA+XIuQ0CzFmQLtgNFIpxRmDKLZrjEO5NSaeStZRgi+WVkgfOnvQvWtFK8/djeXfb4f3BnquCRKjm/cOjtdqBHcFq7Fzlyh0U64XXcJPPgBX3hLNK7mr0ZBCwUDOUs0Xna0a/2FHsjESPX7grZ9J1fbkXRwUrSJ04zbeijqROkjni7wokT+HiWtVcZ/HLOECgYEA0/CykuniBAE+Oqt+qj1OEQSnyiENJffnSzeOOjI17ygxcXYFxprEVjYL1ZVj28appGqs4tueNRoxl7jf4jpD6sDMM32scwmUcCA6K3yqk/F/WVj4MYdn7ETtACCZ9m08EUzksrWOaAb86jqm7k1+mM5xu6lbN3dSua1lKxsNFi0CgYEAtwrwuVupz+GmMna7DtW9S2q0ZwgSoUEcELqcXq2cwAPutrd4rh/xS1xsZZNGqnyT1b3/6vFT2zAyxUiL3o+bhDEEv5HcRJ2rugIhUq2dPAJMYf+kBAFONakir2wHwUUXIkaEHecwxsaI8rsTnXDW+anbJogZSLNcFOBydOaBLe8CgYBKDZknsibxxUlsEI4Ch8cmNR03iBLjCFq9sly0wuSLetzDyzw7Z8pgYSQDbd5lZWXS+B8OaTQ/U7auT7+SeU9P0CvJdgjybQ97mhcZKMclSEV5/5dBHxHVwUOaPsntC7/oP5jNRJjMilyGrxWywEsSs1eT/ZnMqJm0HPzzcdFBxQKBgQAMOkTefQsZAf/yOxA/63NbyGMIxvdHomvXij/L61kfUqPtvM/pAeVCnYf4OSBtXykZDDo+XaS2bb/WggQl9/3xlLy2d235f3brVB0ZwtNQIO8tVMCGK/gniYbxpQvXk1/6QC+vN7SAct7PKEQlLlaOExS6vDjELIcoNd4vhP54LQKBgQAOE7+S2AdjUEVXqWBPnlidZV08HMduy3yZ7mblCX0dA07/ozuM/WOfk92cgqcLddOmsbYdTuqTq1sB98v1QN1RwYe9Eiw0dmfbFlQcXT1YAZVfHNQOMO2xI4gJtkNGUqg1HQsYghOO2wkNHSMCNsoq9GWeIzCF2q6/7oH74jfoaQ==-----END RSA PRIVATE KEY-----");
 
 		EcRepository r = new EcRepository();
-		r.selectedServer = "http://skyrepo.service.eduworks.com";
+		r.selectedServer = "http://localhost:9722/api/custom/";
 
 		EcFile f = new EcFile();
 		f.name = "My_File.txt";
@@ -340,8 +340,12 @@ public class EcEncryptedValueTest
 			r.search("@id:\"" + f.shortId() + "\"", eachCallback, success, failure);
 			console.log("_all Search, searching for signature 1 using signature 2 signatureSheet.");
 			r.search("\""+ppk.toPk().toPem()+"\"", eachCallback, success, failure);
-			console.log("_all Search, searching for signature 2 using signature 2 signatureSheet.");
-			r.search("\""+ppk2.toPk().toPem()+"\"", eachCallback, success, failure);
+			
+			//
+			// This test should not work because sig2 is a @reader on the file and this is an _all search, where the @reader field is not included
+			//
+			//console.log("_all Search, searching for signature 2 using signature 2 signatureSheet.");
+			//r.search("\""+ppk2.toPk().toPem()+"\"", eachCallback, success, failure);
 
 			EcIdentityManager.ids = new Array<EcIdentity>();
 			console.log("ID Search.");
---
+++ ebac.repository/src/test/java/org/cassproject/schema/ebac/EcRepositoryTest.java
@@ -0,0 +1,201 @@
+package org.cassproject.schema.ebac;
+
+import static org.stjs.javascript.Global.console;
+
+
+
+
+
+
+
+
+
+import org.cassproject.ebac.identity.EcIdentity;
+import org.cassproject.ebac.identity.EcIdentityManager;
+import org.cassproject.ebac.repository.EcRepository;
+import org.cassproject.schema.general.EcFile;
+import org.cassproject.schema.general.EcRemoteLinkedData;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.stjs.javascript.Array;
+import org.stjs.javascript.Global;
+import org.stjs.javascript.functions.Callback1;
+import org.stjs.testing.driver.STJSTestDriverRunner;
+
+import com.eduworks.ec.crypto.EcPpk;
+import com.eduworks.ec.random.EcRandom;
+import com.eduworks.ec.remote.EcRemote;
+
+
+@RunWith(STJSTestDriverRunner.class)
+public class EcRepositoryTest
+{
+	String server = "http://localhost:9722/api/custom/";
+	
+	@Test
+	public void createPublicFileTest()
+	{
+		EcRemote.async = false;
+		
+		EcFile thing = new EcFile();
+		thing.id = server;
+		if (!thing.id.endsWith("/"))
+			thing.id += "/";
+		thing.id += "data/";
+		thing.id += thing.type.replace("http://", "").replaceAll("/", ".");
+		thing.id += "/";
+		thing.id += "test-public-object";
+		thing.id += "/";
+		thing.name = "Testing Public Object";
+		
+		
+		console.log("Saving Public Object...");
+		EcRepository.save(thing, new Callback1<String>()
+		{
+			@Override
+			public void $invoke(String p1)
+			{
+				console.log("Saved.");
+			}
+		}, new Callback1<String>()
+		{
+			@Override
+			public void $invoke(String p1)
+			{
+				console.log("Failed to save.");
+				console.log(p1);
+				Assert.fail("Failed to save object.");
+			}
+		});
+		
+		console.log("Retrieving Public Object...");
+		EcRepository.get(thing.shortId(), new Callback1<EcRemoteLinkedData>()
+		{
+			@Override
+			public void $invoke(EcRemoteLinkedData p1)
+			{
+				EcFile retrieved = (EcFile) p1;
+				
+				if(retrieved.owner != null)
+					Assert.assertEquals("File is not Public, has an owner", retrieved.owner.$length(), 0);
+				Assert.assertEquals("Name Does Not Match Saved Object Name", thing.name, retrieved.name);
+				Assert.assertEquals("Name Does Not Match Saved Object Name", thing.id, retrieved.id);
+				
+				console.log("Retrieved Unchanged.");
+			}
+		}, new Callback1<String>()
+		{
+			@Override
+			public void $invoke(String p1)
+			{
+				console.log("Failed to retrieve");
+				console.log(p1);
+				Assert.fail("Failed to retrieve public object after save.");
+			}
+		});
+		
+		console.log("Trying to Delete...");
+		EcRepository._delete(thing, new Callback1<String>()
+		{
+			@Override
+			public void $invoke(String p1)
+			{
+				Assert.fail("Deleted a Public Object, this shouldn't be allowed!");
+			}
+		}, new Callback1<String>()
+		{
+			@Override
+			public void $invoke(String p1)
+			{
+				console.log("Good, Cannot Delete Public Object.");
+				console.log(p1);
+			}
+		});
+	}
+	
+	@Test
+	public void createAndDeleteOwnedFileTest()
+	{
+		EcRemote.async = false;
+		
+		EcPpk ppk = EcPpk
+				.fromPem("-----BEGIN RSA PRIVATE KEY-----MIIEpAIBAAKCAQEAz4BiFucFE9bNcKfGD+e6aPRHl402YM4Z6nrurDRNlnwsWpsCoZasPLkjC314pVtHAI2duZo+esGKDloBsiLxASRJo3R2XiXVh2Y8U1RcHA5mWL4tMG5UY2d0libpNEHbHPNBmooVYpA2yhxN/vGibIk8x69uZWxJcFOxOg6zWG8EjF8UMgGnRCVSMTY3THhTlfZ0cGUzvrfb7OvHUgdCe285XkmYkj/V9P/m7hbWoOyJAJSTOm4/s6fIKpl72lblfN7bKaxTCsJp6/rQdmUeo+PIaa2lDOfo7dWbuTMcqkZ93kispNfYYhsEGUGlCsrrVWhlve8MenO4GdLsFP+HRwIDAQABAoIBAGaQpOuBIYde44lNxJ7UAdYi+Mg2aqyK81Btl0/TQo6hriLTAAfzPAt/z4y8ZkgFyCDD3zSAw2VWCPFzF+d/UfUohKWgyWlb9iHJLQRbbHQJwhkXV6raviesWXpmnVrROocizkie/FcNxac9OmhL8+cGJt7lHgJP9jTpiW6TGZ8ZzM8KBH2l80x9AWdvCjsICuPIZRjc706HtkKZzTROtq6Z/F4Gm0uWRnwAZrHTRpnh8qjtdBLYFrdDcUoFtzOM6UVRmocTfsNe4ntPpvwY2aGTWY7EmTj1kteMJ+fCQFIS+KjyMWQHsN8yQNfD5/j2uv6/BdSkO8uorGSJT6DwmTECgYEA8ydoQ4i58+A1udqA+fujM0Zn46++NTehFe75nqIt8rfQgoduBam3lE5IWj2U2tLQeWxQyr1ZJkLbITtrAI3PgfMnuFAii+cncwFo805Fss/nbKx8K49vBuCEAq3MRhLjWy3ZvIgUHj67jWvl50dbNqc7TUguxhS4BxGr/cPPkP0CgYEA2nbJPGzSKhHTETL37NWIUAdU9q/6NVRISRRXeRqZYwE1VPzs2sIUxA8zEDBHX7OtvCKzvZy1Lg5Unx1nh4nCEVkbW/8npLlRG2jOcZJF6NRfhzwLz3WMIrP6j9SmjJaB+1mnrTjfsg36tDEPDjjJLjJHCx9z/qRJh1v4bh4aPpMCgYACG31T2IOEEZVlnvcvM3ceoqWT25oSbAEBZ6jSLyWmzOEJwJK7idUFfAg0gAQiQWF9K+snVqzHIB02FIXA43nA7pKRjmA+RiqZXJHEShFgk1y2HGiXGA8mSBvcyhTTJqbBy4vvjl5eRLzrZNwBPSUVPC3PZajCHrvZk9WhxWivIQKBgQCzCu1MH2dy4R7ZlqsIJ8zKweeJMZpfQI7pjclO0FTrhh7+Yzd+5db9A/P2jYrBTVHSwaILgTYf49DIguHJfEZXz26TzB7iapqlWxTukVHISt1ryPNo+E58VoLAhChnSiaHJ+g7GESE+d4A9cAACNwgh0YgQIvhIyW70M1e+j7KDwKBgQDQSBLFDFmvvTP3sIRAr1+0OZWd1eRcwdhs0U9GwootoCoUP/1Y64pqukT6B9oIB/No9Nyn8kUX3/ZDtCslaGKEUGMJXQ4hc5J+lq0tSi9ZWBdhqOuMPEfUF3IxW+9yeILP4ppUBn1m5MVOWg5CvuuEeCmy4bhMaUErUlHZ78t5cA==-----END RSA PRIVATE KEY-----");
+		
+		EcFile thing = new EcFile();
+		thing.generateId("http://localhost:9722/api/custom/");
+		thing.name = "Testing Owned Object";
+		
+		thing.addOwner(ppk.toPk());
+		thing.signWith(ppk);
+		
+		EcIdentity newId1 = new EcIdentity();
+		newId1.ppk = ppk;
+
+		EcIdentityManager.ids = new Array<EcIdentity>();
+		EcIdentityManager.addIdentity(newId1);
+		
+		console.log("Saving...");
+		EcRepository.save(thing, new Callback1<String>()
+		{
+			@Override
+			public void $invoke(String p1)
+			{
+				console.log("Saved.");
+			}
+		}, new Callback1<String>()
+		{
+			@Override
+			public void $invoke(String p1)
+			{
+				console.log("Failed to save.");
+				console.log(p1);
+				Assert.fail("Failed to save object.");
+			}
+		});
+		
+		console.log("Retrieving...");
+		EcRepository.get(thing.shortId(), new Callback1<EcRemoteLinkedData>()
+		{
+			@Override
+			public void $invoke(EcRemoteLinkedData p1)
+			{	
+				EcFile retrieved = (EcFile) p1;
+				
+				Assert.assertTrue("Object is not Owned by the Identity that Created It", retrieved.canEdit(newId1.ppk.toPk()));
+				Assert.assertEquals("Name Does Not Match Saved Object Name", thing.name, retrieved.name);
+				Assert.assertEquals("Name Does Not Match Saved Object Name", thing.id, retrieved.id);
+				
+				console.log("Retrieved Unchanged");
+			}
+		}, new Callback1<String>()
+		{
+			@Override
+			public void $invoke(String p1)
+			{
+				console.log("Failed to retrieve.");
+				console.log(p1);
+				Assert.fail("Failed to retrieve object after save.");
+			}
+		});
+		
+		console.log("Deleting...");
+		EcRepository._delete(thing, new Callback1<String>()
+		{
+			@Override
+			public void $invoke(String p1)
+			{
+				console.log("Deleted the Owned Object.");
+			}
+		}, new Callback1<String>()
+		{
+			@Override
+			public void $invoke(String p1)
+			{
+				console.log(p1);
+				Assert.fail("Failed to Delete the Owned Object from Repository");
+			}
+		});
+	}
+
+}
