7e52bd5c05323de3ab03a9f23a205b8f39859257
Devlin Junker <devlin.junker@eduworks.com>
2016-03-31 19:32:44 +0000

    Moved the Server declaration for the tests to one location that can be changed at the top of the file.

--- ebac.repository/src/test/java/org/cassproject/schema/ebac/EcEncryptedValueTest.java
+++ ebac.repository/src/test/java/org/cassproject/schema/ebac/EcEncryptedValueTest.java
@@ -27,6 +27,8 @@ import com.eduworks.schema.ebac.EbacEncryptedValue;
 @ScriptsBefore({ "/forge/forge.bundle.js" })
 public class EcEncryptedValueTest
 {
+	public static String server = "http://localhost:9722/api/custom/";
+	
 	@Test
 	public void encryptDecryptTest()
 	{
@@ -38,7 +40,7 @@ public class EcEncryptedValueTest
 		f.name = "My_File.txt";
 		f.mimeType = "text/plain";
 		f.data = "BASE64ENCODEDDATA";
-		f.generateId("http://sandbox.service.cassproject.org");
+		f.generateId(server);
 		f.checksum = "ABC123";
 		f.addOwner(ppk.toPk());
 		f.signWith(ppk);
@@ -78,7 +80,7 @@ public class EcEncryptedValueTest
 		f.name = "My_File.txt";
 		f.mimeType = "text/plain";
 		f.data = "BASE64ENCODEDDATA";
-		f.generateId("http://skyrepo.service.eduworks.com");
+		f.generateId(server);
 		f.checksum = "ABC123";
 		f.addOwner(ppk.toPk());
 		f.signWith(ppk);
@@ -158,7 +160,7 @@ public class EcEncryptedValueTest
 		f.name = "My_File.txt";
 		f.mimeType = "text/plain";
 		f.data = "BASE64ENCODEDDATA";
-		f.generateId("http://skyrepo.service.eduworks.com");
+		f.generateId(server);
 		f.checksum = "ABC123";
 		f.addOwner(ppk.toPk());
 		f.signWith(ppk);
@@ -237,7 +239,7 @@ public class EcEncryptedValueTest
 				.fromPem("-----BEGIN RSA PRIVATE KEY-----MIIEpAIBAAKCAQEAl4oeWRsroJqAR8UXkmkhDHMcRaDM6a9wsFWNkokyugMevv5TtwHVrm9HMJqVlXn1U1AiVS1rEYB7Iavi2IE7e8WV6GkLrGTrce+YcqhrwF3g104ZEuIC2aFCcLrxvXbSqmmuddS3lRMEwqdWkYIS7MKRSKUBpbydl8wScYxjzUSMNzQrvgUpNc//N7GkpEFXYLjYmVWhYdUp578nw/K9StRqTfku8U+Ub86U0mHXowms4spV8IoOlV53OLpOL+8QwcQYZ+koWL4B0rhz6cvelvBAFygx94t8IzZtRiBniaTF+1nBjyj4+R8PdprxFNnM+S2IQBqUdBKJ3oDoKMqdAwIDAQABAoIBAC3BWmB1P7sCa3FAJVnjvELSDttHLhfxDQlxC4oPOu3HO2VXzVcYirhciRY31qqHZHd/Xp5xVD64mHUWPSw5+QfqJNVDwm6PGjgQq+sSx1YSAm1/+zokW8/yTOlUyOD4G6uwtSiGzdeJIorTk+PjbmtmZA+XIuQ0CzFmQLtgNFIpxRmDKLZrjEO5NSaeStZRgi+WVkgfOnvQvWtFK8/djeXfb4f3BnquCRKjm/cOjtdqBHcFq7Fzlyh0U64XXcJPPgBX3hLNK7mr0ZBCwUDOUs0Xna0a/2FHsjESPX7grZ9J1fbkXRwUrSJ04zbeijqROkjni7wokT+HiWtVcZ/HLOECgYEA0/CykuniBAE+Oqt+qj1OEQSnyiENJffnSzeOOjI17ygxcXYFxprEVjYL1ZVj28appGqs4tueNRoxl7jf4jpD6sDMM32scwmUcCA6K3yqk/F/WVj4MYdn7ETtACCZ9m08EUzksrWOaAb86jqm7k1+mM5xu6lbN3dSua1lKxsNFi0CgYEAtwrwuVupz+GmMna7DtW9S2q0ZwgSoUEcELqcXq2cwAPutrd4rh/xS1xsZZNGqnyT1b3/6vFT2zAyxUiL3o+bhDEEv5HcRJ2rugIhUq2dPAJMYf+kBAFONakir2wHwUUXIkaEHecwxsaI8rsTnXDW+anbJogZSLNcFOBydOaBLe8CgYBKDZknsibxxUlsEI4Ch8cmNR03iBLjCFq9sly0wuSLetzDyzw7Z8pgYSQDbd5lZWXS+B8OaTQ/U7auT7+SeU9P0CvJdgjybQ97mhcZKMclSEV5/5dBHxHVwUOaPsntC7/oP5jNRJjMilyGrxWywEsSs1eT/ZnMqJm0HPzzcdFBxQKBgQAMOkTefQsZAf/yOxA/63NbyGMIxvdHomvXij/L61kfUqPtvM/pAeVCnYf4OSBtXykZDDo+XaS2bb/WggQl9/3xlLy2d235f3brVB0ZwtNQIO8tVMCGK/gniYbxpQvXk1/6QC+vN7SAct7PKEQlLlaOExS6vDjELIcoNd4vhP54LQKBgQAOE7+S2AdjUEVXqWBPnlidZV08HMduy3yZ7mblCX0dA07/ozuM/WOfk92cgqcLddOmsbYdTuqTq1sB98v1QN1RwYe9Eiw0dmfbFlQcXT1YAZVfHNQOMO2xI4gJtkNGUqg1HQsYghOO2wkNHSMCNsoq9GWeIzCF2q6/7oH74jfoaQ==-----END RSA PRIVATE KEY-----");
 
 		EcRepository r = new EcRepository();
-		r.selectedServer = "http://localhost:9722/api/custom/";
+		r.selectedServer = server;
 
 		EcFile f = new EcFile();
 		f.name = "My_File.txt";
