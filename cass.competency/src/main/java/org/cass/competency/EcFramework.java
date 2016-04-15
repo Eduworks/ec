450a2cc69bffb25443cc4cf36b2d373d4baec354
Devlin Junker <devlin.junker@eduworks.com>
2016-04-15 14:36:35 +0000

    Updated remove Competency so it only calls the success callback after all relationships and levels related to that competency are removed. Updated removeRelationship and removeLevel so they dont error if no callback is given.

--- cass.competency/src/main/java/org/cass/competency/EcFramework.java
+++ cass.competency/src/main/java/org/cass/competency/EcFramework.java
@@ -4,6 +4,8 @@ import org.cassproject.ebac.repository.EcRepository;
 import org.cassproject.schema.cass.competency.Framework;
 import org.cassproject.schema.general.EcRemoteLinkedData;
 import org.stjs.javascript.Array;
+import org.stjs.javascript.JSCollections;
+import org.stjs.javascript.Map;
 import org.stjs.javascript.functions.Callback1;
 
 public class EcFramework extends Framework
@@ -19,30 +21,64 @@ public class EcFramework extends Framework
 		competency.push(id);
 	}
 
-	public void removeCompetency(String id2, final Callback1<String> success, final Callback1<String> failure)
+	static Map<String, Boolean> relDone = JSCollections.$map();
+	static Map<String, Boolean> levelDone = JSCollections.$map();
+			
+	public void removeCompetency(final String id, final Callback1<String> success, final Callback1<String> failure)
 	{
-		final String id = trimVersionFromUrl(id2);
+		final String shortId = trimVersionFromUrl(id);
 		if (competency == null)
 			competency = new Array<String>();
 		for (int i = 0; i < competency.$length(); i++)
-			if (competency.$get(i).equals(id))
+			if (competency.$get(i).equals(shortId) || competency.$get(i).equals(id))
 				competency.splice(i, 1);
 		if (relation == null && level == null)
-			success.$invoke("");
+			if(success != null)
+				success.$invoke("");
+		
+		relDone.$put(id, false);
+		levelDone.$put(id, false);
+
 		if (relation != null)
 		{
-			removeRelationshipsThatInclude(id, 0, success, failure);
+			removeRelationshipsThatInclude(id, 0, new Callback1<String>() {
+				@Override
+				public void $invoke(String p1) {
+					if(levelDone.$get(id)){
+						if(success != null)
+							success.$invoke(p1);
+					}else{
+						relDone.$put(id, true);
+					}
+				}
+			}, failure);
+		}else{
+			relDone.$put(id, true);
 		}
 		if (level != null)
 		{
-			removeLevelsThatInclude(id, 0, success, failure);
+			removeLevelsThatInclude(id, 0, new Callback1<String>() {
+				@Override
+				public void $invoke(String p1) {
+					if(relDone.$get(id)){
+						if(success != null)
+							success.$invoke(p1);
+					}else{
+						levelDone.$put(id, true);
+					}
+				}
+			}, failure);
+		}
+		else{
+			levelDone.$put(id, true);
 		}
 	}
 
 	private void removeRelationshipsThatInclude(String id, int i, final Callback1<String> success, final Callback1<String> failure)
 	{
+		final String shortId = trimVersionFromUrl(id);
 		EcFramework me = this;
-		if (i >= relation.$length())
+		if (i >= relation.$length() && success != null)
 			success.$invoke("");
 		else
 			EcRepository.get(relation.$get(i), new Callback1<EcRemoteLinkedData>()
@@ -52,7 +88,7 @@ public class EcFramework extends Framework
 				{
 					EcAlignment a = new EcAlignment();
 					a.copyFrom(p1);
-					if (a.source == id || a.target == id)
+					if (a.source == shortId || a.target == shortId || a.source == id || a.target == id)
 					{
 						me.relation.splice(i, 1);
 						me.removeRelationshipsThatInclude(id, i, success, failure);
@@ -64,9 +100,10 @@ public class EcFramework extends Framework
 	}
 
 	private void removeLevelsThatInclude(String id, int i, final Callback1<String> success, final Callback1<String> failure)
-	{
+	{ 
+		final String shortId = trimVersionFromUrl(id);
 		EcFramework me = this;
-		if (i >= level.$length())
+		if (i >= level.$length() && success != null)
 			success.$invoke("");
 		else
 			EcRepository.get(level.$get(i), new Callback1<EcRemoteLinkedData>()
@@ -76,7 +113,7 @@ public class EcFramework extends Framework
 				{
 					EcLevel a = new EcLevel();
 					a.copyFrom(p1);
-					if (a.competency == id)
+					if (a.competency == shortId || a.competency == id)
 					{
 						me.level.splice(i, 1);
 						me.removeLevelsThatInclude(id, i, success, failure);
