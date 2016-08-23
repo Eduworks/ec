cc1b50967045edce603291cefe985c27d48d15ba
Tom Buskirk <tom.buskirk@eduworks.com>
2016-08-23 21:31:30 +0000

    T. Buskirk: Added results processing

--- cass.rollup/src/main/java/cass/rollup/EvidenceProcessor.java
+++ cass.rollup/src/main/java/cass/rollup/EvidenceProcessor.java
@@ -19,6 +19,7 @@ import com.eduworks.ec.crypto.EcPk;
 import com.eduworks.schema.ebac.EbacSignature;
 
 import cass.rollup.InquiryPacket.IPType;
+import cass.rollup.InquiryPacket.ResultType;
 
 public class EvidenceProcessor
 {
@@ -63,7 +64,8 @@ public class EvidenceProcessor
 		   processChildPackets(ip.equivalentPackets);	
 		   processChildPackets(ip.subPackets);
 		}
-		//TODO process packet results
+		//determine result
+		determineResult(ip);
 	}
 	
 	private void processChildPackets(Array<InquiryPacket> childPackets) 
@@ -261,5 +263,159 @@ public class EvidenceProcessor
       }
       ip.hasCheckedRollupRulesForCompetency = true;
    }
+
+   /**IF IP type is COMBINATOR_AND:
+    *   IF any equivalent packets = INDETERMINANT THEN INDETERMINANT
+    *   ELSE IF any sub packets = INDETERMINANT THEN INDETERMINANT    
+    *   ELSE IF all equivalent packets = UNKNOWN && all sub packets = UNKNOWN THEN UNKNOWN
+    *   ELSE IF any equivalent packets = FALSE THEN FALSE 
+    *   ELSE IF any sub packets = FALSE THEN FALSE
+    *   ELSE TRUE
+    **/ 
+   private void determineCombinatorAndResult(InquiryPacket ip)
+   {
+      if (ip.anyIndeterminantChildPackets()) ip.result = ResultType.INDETERMINANT;
+      else if (ip.allChildPacketsUnknown()) ip.result = ResultType.UNKNOWN;
+      else if (ip.anyChildPacketsAreFalse()) ip.result = ResultType.FALSE;
+      else ip.result = ResultType.TRUE;
+   }
+   
+   /**
+    *   IF IP type is COMBINATOR_OR:
+    *   IF any equivalent packets = INDETERMINANT THEN INDETERMINANT
+    *   ELSE IF any sub packets = INDETERMINANT THEN INDETERMINANT  
+    *   ELSE IF all equivalent packets = UNKNOWN && all sub packets = UNKNOWN THEN UNKNOWN
+    *   ELSE IF any equivalent packets = TRUE THEN TRUE 
+    *   ELSE IF any sub packets = TRUE THEN TRUE
+    *   ELSE FALSE
+    **/
+   private void determineCombinatorOrResult(InquiryPacket ip)
+   {
+      if (ip.anyIndeterminantChildPackets()) ip.result = ResultType.INDETERMINANT;
+      else if (ip.allChildPacketsUnknown()) ip.result = ResultType.UNKNOWN;
+      else if (ip.anyChildPacketsAreTrue()) ip.result = ResultType.TRUE;
+      else ip.result = ResultType.FALSE;
+   }
+   
+   /**
+    * IF IP type is COMPETENCY|ROLLUPRULE:
+    *   assertionResult = (
+    *       IF number of positive assertions > 0 && number of negative assertions = 0 THEN assertionResult = TRUE 
+    *       IF number of positive assertions = 0 && number of negative assertions > 0 THEN assertionResult = FALSE
+    *       IF number of positive assertions > 0 && number of negative assertions > 0 THEN assertionResult = INDETERMINANT
+    *       IF number of positive assertions = 0 && number of negative assertions = 0 THEN assertionResult = UNKNOWN
+    *   ) 
+    * 
+    *   IF assertionResult = INDETERMINANT THEN INDETERMINANT
+    *   ELSE IF any equivalent packets = INDETERMINANT THEN INDETERMINANT
+    *   ELSE IF any sub packets = INDETERMINANT THEN INDETERMINANT  
+    *   
+    *   ELSE IF assertionResult = UNKNOWN:
+    *     IF all equivalent packets = UNKNOWN
+    *        IF all sub packets = UNKNOWN THEN UNKNOWN
+    *        IF all sub packets = TRUE|UNKNOWN THEN TRUE
+    *        IF all sub packets = FALSE|UNKNOWN THEN FALSE
+    *        ELSE INDETERMINANT
+    *     
+    *     ELSE IF all equivalent packets = TRUE|UNKNOWN
+    *        IF all sub packets = TRUE|UNKNOWN THEN TRUE
+    *        ELSE INDETERMINANT
+    *        
+    *     ELSE IF all equivalent packets = FALSE|UNKNOWN
+    *        IF all sub packets = FALSE|UNKNOWN THEN FALSE
+    *        ELSE INDETERMINANT
+    *        
+    *     ELSE INDETERMINANT
+    *        
+    *   
+    *   ELSE IF assertionResult = TRUE:
+    *     IF all equivalent packets = TRUE|UNKNOWN
+    *        IF all sub packets = TRUE|UNKNOWN THEN TRUE
+    *        ELSE INDETERMINANT
+    *        
+    *     ELSE INDETERMINANT
+    *     
+    *   ELSE IF assertionResult = FALSE:
+    *     IF all equivalent packets = FALSE|UNKNOWN
+    *        IF all sub packets = FALSE|UNKNOWN THEN FALSE
+    *        ELSE INDETERMINANT
+    *        
+    *     ELSE INDETERMINANT
+    *   
+    **/
+   private ResultType getPacketAssertionResult(InquiryPacket ip)
+   {
+      if (ip.positive.$length() > 0 && ip.negative.$length() == 0) return ResultType.TRUE;
+      else if (ip.positive.$length() == 0 && ip.negative.$length() > 0) return ResultType.FALSE;
+      else if (ip.positive.$length() > 0 && ip.negative.$length() > 0) return ResultType.INDETERMINANT;
+      else return ResultType.UNKNOWN;
+   }
+   
+   private void determineResultForUnknownAssertionResult(InquiryPacket ip)
+   {    
+      if (ip.allChildPacketsUnknown()) ip.result = ResultType.UNKNOWN;
+      else if (ip.allEquivalentPacketsUnknown())
+      {
+         if (ip.allSubPacketsTrueOrUnknown()) ip.result = ResultType.TRUE;
+         else if (ip.allEquivalentPacketsFalseOrUnknown()) ip.result = ResultType.FALSE;
+         else ip.result = ResultType.INDETERMINANT;
+      }
+      else if (ip.allEquivalentPacketsTrueOrUnknown())
+      {
+         if (ip.allSubPacketsTrueOrUnknown()) ip.result = ResultType.TRUE;
+         else ip.result = ResultType.INDETERMINANT;
+      }
+      else if (ip.allEquivalentPacketsFalseOrUnknown())
+      {
+         if (ip.allSubPacketsFalseOrUnknown()) ip.result = ResultType.FALSE;
+         else ip.result = ResultType.INDETERMINANT;
+      }
+      else ip.result = ResultType.INDETERMINANT;
+   }
+   
+   private void determineResultForTrueAssertionResult(InquiryPacket ip)
+   {
+      if (ip.allEquivalentPacketsTrueOrUnknown()) 
+      {
+         if (ip.allSubPacketsTrueOrUnknown()) ip.result = ResultType.TRUE;
+         else ip.result = ResultType.INDETERMINANT;
+      }
+      else ip.result = ResultType.INDETERMINANT;
+   }
+   
+   private void determineResultForFalseAssertionResult(InquiryPacket ip)
+   {
+      if (ip.allEquivalentPacketsFalseOrUnknown())
+      {
+         if (ip.allSubPacketsFalseOrUnknown()) ip.result = ResultType.FALSE;
+         else ip.result = ResultType.INDETERMINANT;
+      }
+      else ip.result = ResultType.INDETERMINANT;
+   }
+   
+   private void determineCompetencyRollupRuleResult(InquiryPacket ip)
+   {
+      ResultType assertionResult = getPacketAssertionResult(ip);
+      if (ResultType.INDETERMINANT.equals(assertionResult) || ip.anyIndeterminantChildPackets()) ip.result = ResultType.INDETERMINANT;
+      else if (ResultType.UNKNOWN.equals(assertionResult)) determineResultForUnknownAssertionResult(ip);
+      else if (ResultType.TRUE.equals(assertionResult)) determineResultForTrueAssertionResult(ip);
+      else determineResultForFalseAssertionResult(ip);    
+   }
+   
+   private void determineResult(InquiryPacket ip) 
+   {
+      if (ip.numberOfQueriesRunning == 0) 
+      {
+         if (IPType.COMBINATOR_AND.equals(ip.type)) determineCombinatorAndResult(ip);
+         else if (IPType.COMBINATOR_OR.equals(ip.type)) determineCombinatorOrResult(ip);
+         else determineCompetencyRollupRuleResult(ip);
+      }
+      else 
+      {
+         log(ip,"Trying to determine result...running query count: " + ip.numberOfQueriesRunning);
+         determineResult(ip);
+      }
+   }
    
 }
+
--- cass.rollup/src/main/java/cass/rollup/InquiryPacket.java
+++ cass.rollup/src/main/java/cass/rollup/InquiryPacket.java
@@ -15,6 +15,7 @@ public class InquiryPacket
 {
    
    public enum IPType {COMPETENCY, ROLLUPRULE, COMBINATOR_AND, COMBINATOR_OR}
+   public enum ResultType {TRUE, FALSE, UNKNOWN, INDETERMINANT}
    
 	public Array<EcPk> subject;
 	public EcCompetency competency;
@@ -51,6 +52,8 @@ public class InquiryPacket
 	
 	public String rule;
 	
+	public ResultType result;
+	
 	public InquiryPacket(Array<EcPk> subject, EcCompetency competency, EcLevel level, EcFramework context, 
 	                     Callback1<InquiryPacket> success, Callback1<String> failure, String rule, IPType type)
 	{
@@ -74,5 +77,106 @@ public class InquiryPacket
 	{
 		return context;		
 	}
+	
+	public boolean anyIndeterminantChildPackets() 
+	{
+	   for (int i=0;i<equivalentPackets.$length();i++)
+	   {
+	      if (ResultType.INDETERMINANT.equals(equivalentPackets.$get(i).result)) return true;
+	   }
+	   for (int i=0;i<subPackets.$length();i++)
+      {
+         if (ResultType.INDETERMINANT.equals(subPackets.$get(i).result)) return true;
+      }
+	   return false;
+	}
+	
+	public boolean allChildPacketsUnknown() 
+   {
+      for (int i=0;i<equivalentPackets.$length();i++)
+      {
+         if (!ResultType.UNKNOWN.equals(equivalentPackets.$get(i).result)) return false;
+      }
+      for (int i=0;i<subPackets.$length();i++)
+      {
+         if (!ResultType.UNKNOWN.equals(subPackets.$get(i).result)) return false;
+      }
+      return true;
+   }
+	
+	public boolean anyChildPacketsAreFalse()
+	{
+	   for (int i=0;i<equivalentPackets.$length();i++)
+      {
+         if (ResultType.FALSE.equals(equivalentPackets.$get(i).result)) return true;
+      }
+      for (int i=0;i<subPackets.$length();i++)
+      {
+         if (!ResultType.FALSE.equals(subPackets.$get(i).result)) return true;
+      }
+      return false;
+	}
+	
+	public boolean anyChildPacketsAreTrue()
+   {
+      for (int i=0;i<equivalentPackets.$length();i++)
+      {
+         if (ResultType.TRUE.equals(equivalentPackets.$get(i).result)) return true;
+      }
+      for (int i=0;i<subPackets.$length();i++)
+      {
+         if (!ResultType.TRUE.equals(subPackets.$get(i).result)) return true;
+      }
+      return false;
+   }
+	
+	public boolean allEquivalentPacketsUnknown()
+   {
+      for (int i=0;i<equivalentPackets.$length();i++)
+      {
+         if (!ResultType.UNKNOWN.equals(equivalentPackets.$get(i).result)) return false;
+      }
+      return true;
+   }
+	
+	public boolean allEquivalentPacketsTrueOrUnknown()
+	{
+	   for (int i=0;i<equivalentPackets.$length();i++)
+      {
+         if (ResultType.FALSE.equals(equivalentPackets.$get(i).result) ||
+             ResultType.INDETERMINANT.equals(equivalentPackets.$get(i).result)) return false;
+      }
+	   return true;
+	}
+	
+	public boolean allSubPacketsTrueOrUnknown()
+   {
+      for (int i=0;i<subPackets.$length();i++)
+      {
+         if (ResultType.FALSE.equals(subPackets.$get(i).result) ||
+             ResultType.INDETERMINANT.equals(subPackets.$get(i).result)) return false;
+      }
+      return true;
+   }
+	
+	public boolean allEquivalentPacketsFalseOrUnknown()
+   {
+      for (int i=0;i<equivalentPackets.$length();i++)
+      {
+         if (ResultType.TRUE.equals(equivalentPackets.$get(i).result) ||
+             ResultType.INDETERMINANT.equals(equivalentPackets.$get(i).result)) return false;
+      }
+      return true;
+   }
+	
+	public boolean allSubPacketsFalseOrUnknown()
+   {
+      for (int i=0;i<subPackets.$length();i++)
+      {
+         if (ResultType.TRUE.equals(subPackets.$get(i).result) ||
+             ResultType.INDETERMINANT.equals(subPackets.$get(i).result)) return false;
+      }
+      return true;
+   }
 
 }
