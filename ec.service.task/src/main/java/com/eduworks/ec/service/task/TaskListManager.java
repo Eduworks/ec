e4437fe65f84a76b09e64892578b38f514fd4d44
Devlin Junker <devlin.junker@eduworks.com>
2016-09-16 17:34:56 +0000

    Changed to GET request for readTaskList

--- ec.service.task/src/main/java/com/eduworks/ec/service/task/TaskListManager.java
+++ ec.service.task/src/main/java/com/eduworks/ec/service/task/TaskListManager.java
@@ -8,13 +8,13 @@ import com.eduworks.ec.service.task.model.TaskList;
 
 public class TaskListManager {
 	
-	private static String selectedServer = "http://localhost:9722/api/custom/taskList";
+	private static String selectedServer = "http://localhost:9722/api/custom/taskList/";
 	
 	private static final String READ = "read";
 	
 	public static void readTaskList(Callback1<Object> success, Callback1<String> fail)
 	{
-		EcRemote.postExpectingObject(selectedServer, READ, null, success, fail);
+		EcRemote.getExpectingObject(selectedServer, READ, success, fail);
 	}
 	
 }
