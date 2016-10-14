79048a5764612a032d1758b2d71b99d3ace7c902
Devlin Junker <devlin.junker@eduworks.com>
2016-10-14 20:36:14 +0000

    Added signature sheet to the posted information

--- cass.adapter/src/main/java/org/cass/adapter/XapiConfig.java
+++ cass.adapter/src/main/java/org/cass/adapter/XapiConfig.java
@@ -1,5 +1,6 @@
 package org.cass.adapter;
 
+import org.cassproject.ebac.identity.EcIdentityManager;
 import org.json.ld.EcLinkedData;
 import org.stjs.javascript.Global;
 import org.stjs.javascript.functions.Callback1;
@@ -20,15 +21,32 @@ public class XapiConfig extends EcLinkedData {
 		super(null, null);
 	}
 	
-	public void save(String serverUrl, Callback1<Object> success, Callback1<String> failure){
-		FormData fd = new FormData();
+	public void save(final String serverUrl, final Callback1<Object> success, final Callback1<String> failure){		
+		final FormData fd = new FormData();
 		fd.append("config", Global.JSON.stringify(this));
+		EcIdentityManager.signatureSheetAsync(60000, serverUrl, new Callback1<String>()
+		{
+			@Override
+			public void $invoke(String signatureSheet)
+			{
+				fd.append("signatureSheet", signatureSheet);
+				EcRemote.postExpectingObject(serverUrl, "adapter/xapi/config/set", fd, success, failure);
+			}
+		});
 		
-		EcRemote.postExpectingObject(serverUrl, "adapter/xapi/config/set", fd, success, failure);
 	}
 	
-	public static void get(String serverUrl, Callback1<Object> success, Callback1<String> failure){
-		EcRemote.getExpectingObject(serverUrl, "adapter/xapi/config/get", success, failure);
+	public static void get(final String serverUrl, final Callback1<Object> success, final Callback1<String> failure){
+		final FormData fd = new FormData();
+		EcIdentityManager.signatureSheetAsync(60000, serverUrl,new Callback1<String>()
+		{
+			@Override
+			public void $invoke(String signatureSheet)
+			{
+				fd.append("signatureSheet", signatureSheet);
+				EcRemote.postExpectingObject(serverUrl, "adapter/xapi/config/get", fd, success, failure);
+			}
+		});
 	}
 
 }
