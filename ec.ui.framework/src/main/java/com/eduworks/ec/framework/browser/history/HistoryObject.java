406990496020f3b40bf12d92bb96a9fe8b482390
Devlin Junker <devlin.junker@eduworks.com>
2016-11-09 00:35:06 +0000

    Documentation

--- ec.ui.framework/src/main/java/com/eduworks/ec/framework/browser/history/HistoryObject.java
+++ ec.ui.framework/src/main/java/com/eduworks/ec/framework/browser/history/HistoryObject.java
@@ -6,7 +6,6 @@ import org.stjs.javascript.annotation.SyntheticType;
  * STJS Wrapper for the Browser Native History Object
  * 
  * @author devlin.junker@eduworks.com
- *
  */
 @SyntheticType
 public class HistoryObject {
