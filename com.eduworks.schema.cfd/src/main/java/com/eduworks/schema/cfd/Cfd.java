a86ead4f134fdf0a77e3af935b2841209eb5829a
Debbie Brown <debbie.brown@eduworks.com>
2016-11-21 20:32:37 +0000

    Draft version of cfd extension to competency

---
+++ com.eduworks.schema.cfd/src/main/java/com/eduworks/schema/cfd/Cfd.java
@@ -0,0 +1,10 @@
+package com.eduworks.schema.cfd;
+
+public class Cfd
+{
+
+	public static String context_0_1 = "http://schema.eduworks.com/cass/0.1";
+	public static String context_0_2 = "http://schema.eduworks.com/cass/0.2";
+	public static String context = "http://schema.eduworks.com/cass/0.2";
+
+}
