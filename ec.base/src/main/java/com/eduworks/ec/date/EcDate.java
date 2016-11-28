524b87ed1a33aa07b0c53b690a26641dc854a130
Ronald Ray <fritz.ray@eduworks.com>
2016-11-28 09:59:31 -0800

    Date helper

---
+++ ec.base/src/main/java/com/eduworks/ec/date/EcDate.java
@@ -0,0 +1,13 @@
+package com.eduworks.ec.date;
+
+import org.stjs.javascript.Date;
+import org.stjs.javascript.JSObjectAdapter;
+import org.stjs.javascript.functions.Function0;
+
+public class EcDate
+{
+	public static String toISOString(Date obj)
+	{
+		return (String) ((Function0) JSObjectAdapter.$get(obj, "toISOString")).$invoke();
+	}
+}
