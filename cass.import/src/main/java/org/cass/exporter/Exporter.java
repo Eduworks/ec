74b9aae02f8ddf56e2af2c85802e1668d32505a7
devlin junker <devlin.junker@eduworks.com>
2017-01-25 14:45:07 -0800

    Moved CSV Export to Java, exports all fields too so CFD can be exported
    Import doesn't handle the additional fields in the CSV yet though, so
    they cannot be imported yet even though they are in the output

---
+++ cass.import/src/main/java/org/cass/exporter/Exporter.java
@@ -0,0 +1,14 @@
+package org.cass.exporter;
+
+/**
+ * Base class for all exporters, can hold helper functions 
+ * that are useful for all exporters
+ * 
+ * @module org.cassproject
+ * @class Exporter
+ * @abstract
+ * @author devlin.junker@eduworks.com
+ */
+public abstract class Exporter {
+	
+}
