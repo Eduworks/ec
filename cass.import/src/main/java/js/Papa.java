6ba2c32bbf2152fc6d32fde991d5d8eddb2c3071
devlin junker <devlin.junker@eduworks.com>
2017-01-27 14:25:29 -0800

    fixed missing files

--- cass.import/src/main/java/js/Papa.java
+++ cass.import/src/main/java/js/Papa.java
@@ -1,9 +1,12 @@
 package js;
 
+import org.stjs.javascript.Array;
 import org.stjs.javascript.annotation.STJSBridge;
 
 @STJSBridge( sources = {"/src/main/resources/papaparse.js"})
 public class Papa {
 	
 	public static void parse(Object file, PapaParseParams params){}
+
+	public static String unparse(Array<Object> csvOutput) { return null; }
 }
