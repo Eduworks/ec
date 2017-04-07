9ab34342b8a5afbd0cb613ee73f9abde7d6cda3a
devlin junker <devlin.junker@eduworks.com>
2017-04-07 09:57:09 -0700

    Added new CFD Classes and fixed Assessment and Reference to CreativeWork

---
+++ cfd.competency/src/main/java/com/eduworks/schema/cfd/competency/CfdFramework.java
@@ -0,0 +1,285 @@
+package com.eduworks.schema.cfd.competency;
+
+import org.cass.competency.EcAlignment;
+import org.cass.competency.EcCompetency;
+import org.cass.competency.EcFramework;
+import org.cassproject.ebac.repository.EcEncryptedValue;
+import org.cassproject.ebac.repository.EcRepository;
+import org.cassproject.schema.general.EcRemoteLinkedData;
+import org.stjs.javascript.Array;
+import org.stjs.javascript.Global;
+import org.stjs.javascript.JSCollections;
+import org.stjs.javascript.Map;
+import org.stjs.javascript.functions.Callback0;
+import org.stjs.javascript.functions.Callback1;
+
+public class CfdFramework extends EcFramework {
+	
+	private static Map<String, Integer> toRemove;
+	private static Map<String, Integer> removed;
+	
+	/**
+	 * Deletes this framework from the server specified by it's ID
+	 *
+	 * @memberOf CfdFramework
+	 * @method _delete
+	 * @param {Callback1<String>} success
+	 * 			Callback triggered if successfully deleted framework
+	 * @param {Callback1<String>} failure
+	 * 			Callback triggered if error occurs when deleting the framework
+	 */
+	public void _delete(final Callback1<String> success, final Callback1<String> failure)
+	{
+		if(toRemove == null)
+			toRemove = JSCollections.$map();
+		int remove = 0;
+		remove += (this.competency == null ? 0 : this.competency.$length());
+		remove += (this.relation == null ? 0 : this.relation.$length());
+		toRemove.$put(this.shortId(), remove);
+		
+		if(removed == null)
+			removed = JSCollections.$map();
+		removed.$put(this.shortId(), 0);
+		 
+		final CfdFramework that = this;
+		 
+		final Callback0 onAllRemove = new Callback0(){
+			 public void $invoke(){
+				 EcRepository.DELETE(that, success, failure);
+			 }
+    	 };
+		 
+    	 if(this.competency != null && this.competency.$length() > 0){
+    		 for (int x=0; x<this.competency.$length(); x++) {
+    	 			CfdCompetency.get(this.competency.$get(x), new Callback1<EcCompetency>(){
+    	 				public void $invoke(final EcCompetency comp){
+    	 					comp._delete(new Callback1<String>(){
+    	 		 				public void $invoke(String p1){
+    		 			    	    removed.$put(that.shortId(), removed.$get(that.shortId())+1);
+    	 		 					if(removed.$get(that.shortId()) == toRemove.$get(that.shortId()))
+    		 		        			onAllRemove.$invoke();
+    	 		 				}
+    	 	    			}, new Callback1<String>(){
+    	 	    				public void $invoke(String err){
+    		 	    				String error = "Error deleting competency ("+comp.id+"): "+err;
+    		 			    	    
+    		 	    				failure.$invoke(error);
+    		 	    				
+    		 	    				removed.$put(that.shortId(), removed.$get(that.shortId())+1);
+    	 		 					if(removed.$get(that.shortId()) == toRemove.$get(that.shortId()))
+    		 		        			onAllRemove.$invoke();
+    	 	    				}
+    	 	    			}, null);
+    	 					
+    	 				}
+    	 			},	new Callback1<String>(){
+    	 				public void $invoke(String err){
+    						String error = "Error retrieving competency to delete: "+err;
+    		        		
+    						failure.$invoke(error);
+    		        		
+    						removed.$put(that.shortId(), removed.$get(that.shortId())+1);
+ 		 					if(removed.$get(that.shortId()) == toRemove.$get(that.shortId()))
+	 		        			onAllRemove.$invoke();
+    	 				}
+    				});
+    			 }
+    	 }
+		 
+		 for (int x=0; x<this.relation.$length(); x++) {
+	    	EcAlignment.get(this.relation.$get(x), new Callback1<EcAlignment>(){
+ 				public void $invoke(final EcAlignment rel){
+					rel._delete(new Callback1<String>(){
+		 				public void $invoke(String p1){	    				    	   
+		 					removed.$put(that.shortId(), removed.$get(that.shortId())+1);
+ 		 					if(removed.$get(that.shortId()) == toRemove.$get(that.shortId()))
+	 		        			onAllRemove.$invoke();
+		 				}
+	        		}, new Callback1<String>(){
+	     				public void $invoke(String err){
+		        			String error = "Error deleting relation ("+rel.id+"): "+err;
+			        		
+		        			failure.$invoke(error);
+			        		
+			        		removed.$put(that.shortId(), removed.$get(that.shortId())+1);
+ 		 					if(removed.$get(that.shortId()) == toRemove.$get(that.shortId()))
+	 		        			onAllRemove.$invoke();
+	     				}
+	        		});
+ 				}
+			}, new Callback1<String>(){
+ 				public void $invoke(String err){
+					String error = "Error retrieving relationship to delete: "+err;
+	        		
+					failure.$invoke(error);
+					removed.$put(that.shortId(), removed.$get(that.shortId())+1);
+	 					if(removed.$get(that.shortId()) == toRemove.$get(that.shortId()))
+		        			onAllRemove.$invoke();
+ 				}
+			});
+		 }
+	}
+
+
+	/**
+	 * Retrieves a framework from the server, specified by the ID
+	 *
+	 * @memberOf CfdFramework
+	 * @method get
+	 * @static
+	 * @param {String} id
+	 * 			ID of the framework to retrieve
+	 * @param {Callback1<EcFramework>} success
+	 * 			Callback triggered after successfully retrieving the framework,
+	 * 			returns the retrieved framework
+	 * @param {Callback1<String>} failure
+	 * 			Callback triggered if an error occurs while retrieving the framework
+	 */
+	public static void cfdGet(String id, final Callback1<CfdFramework> success, final Callback1<String> failure)
+	{
+		EcRepository.get(id, new Callback1<EcRemoteLinkedData>()
+		{
+			@Override
+			public void $invoke(EcRemoteLinkedData p1)
+			{
+				CfdFramework framework = new CfdFramework();
+
+				if (p1.isA(EcEncryptedValue.myType))
+				{
+					EcEncryptedValue encrypted = new EcEncryptedValue();
+					encrypted.copyFrom(p1);
+					p1 = encrypted.decryptIntoObject();
+
+					EcEncryptedValue.encryptOnSave(p1.id, true);
+				}
+				if (p1.isAny(framework.getTypes()))
+				{
+					framework.copyFrom(p1);
+
+					if (success != null)
+						success.$invoke(framework);
+				} else
+				{
+					String msg = "Resultant object is not a framework.";
+					if (failure != null)
+						failure.$invoke(msg);
+					else
+						Global.console.error(msg);
+				}
+
+			}
+		}, new Callback1<String>()
+		{
+			@Override
+			public void $invoke(String p1)
+			{
+				if (failure != null)
+					failure.$invoke(p1);
+			}
+		});
+	}
+
+	/**
+	 * Retrieves a framework from the server in a blocking fashion, specified by the ID
+	 *
+	 * @memberOf CfdFramework
+	 * @method getBlocking
+	 * @static
+	 * @param {String} id
+	 * 			ID of the framework to retrieve
+	 * @param {Callback1<EcFramework>} success
+	 * 			Callback triggered after successfully retrieving the framework,
+	 * 			returns the retrieved framework
+	 * @param {Callback1<String>} failure
+	 * 			Callback triggered if an error occurs while retrieving the framework
+	 */
+	public static CfdFramework cfdGetBlocking(String id) {
+		EcRemoteLinkedData p1 = EcRepository.getBlocking(id);
+		if (p1 == null) return null;
+		CfdFramework framework = new CfdFramework();
+
+		if (p1.isA(EcEncryptedValue.myType)) {
+			EcEncryptedValue encrypted = new EcEncryptedValue();
+			encrypted.copyFrom(p1);
+			p1 = encrypted.decryptIntoObject();
+
+			EcEncryptedValue.encryptOnSave(p1.id, true);
+		}
+		if (p1.isAny(framework.getTypes())) {
+			framework.copyFrom(p1);
+			return framework;
+		} else {
+			return null;
+		}
+	}
+
+
+
+	/**
+	 * Searches the repository given for frameworks using the query passed in
+	 *
+	 * @memberOf CfdFramework
+	 * @method search
+	 * @static
+	 * @param {EcRepository} repo
+	 * 			Repository to search for frameworks
+	 * @param {String} query
+	 * 			Query string used to search for a framework
+	 * @param {Callback1<Array<EcFramework>} success
+	 * 			Callback triggered when the search successfully returns,
+	 * 			returns search results
+	 * @param {Callback1<String>} failure
+	 * 			Callback triggered if an error occurs while searching
+	 * @param {Object} paramObj
+	 * 			Parameter object for search
+	 * 		@param size
+	 * 		@param start
+	 */
+	public static void cfdSearch(EcRepository repo, String query, final Callback1<Array<CfdFramework>> success, Callback1<String> failure, Object paramObj)
+	{
+		String queryAdd = "";
+		queryAdd = new EcFramework().getSearchStringByType();
+
+		if (query == null || query == "")
+			query = queryAdd;
+		else
+			query = "(" + query + ") AND " + queryAdd;
+
+		repo.searchWithParams(query, paramObj, null, new Callback1<Array<EcRemoteLinkedData>>()
+		{
+
+			@Override
+			public void $invoke(Array<EcRemoteLinkedData> p1)
+			{
+				if (success != null)
+				{
+					Array<CfdFramework> ret = JSCollections.$array();
+					for (int i = 0; i < p1.$length(); i++)
+					{
+
+						CfdFramework framework = new CfdFramework();
+						if (p1.$get(i).isAny(framework.getTypes()))
+						{
+							framework.copyFrom(p1.$get(i));
+						} else if (p1.$get(i).isA(EcEncryptedValue.myType))
+						{
+							EcEncryptedValue val = new EcEncryptedValue();
+							val.copyFrom(p1.$get(i));
+							if (val.isAnEncrypted(EcFramework.myType))
+							{
+								EcRemoteLinkedData obj = val.decryptIntoObject();
+								framework.copyFrom(obj);
+								EcEncryptedValue.encryptOnSave(framework.id, true);
+							}
+						}
+
+						ret.$set(i, framework);
+					}
+
+					success.$invoke(ret);
+				}
+			}
+
+		}, failure);
+	}
+}
--- cfd.competency/src/main/java/com/eduworks/schema/cfd/competency/CfdReference.java
+++ cfd.competency/src/main/java/com/eduworks/schema/cfd/competency/CfdReference.java
@@ -1,5 +1,15 @@
 package com.eduworks.schema.cfd.competency;
 
+import org.cassproject.ebac.repository.EcEncryptedValue;
+import org.cassproject.ebac.repository.EcRepository;
+import org.cassproject.schema.general.EcRemoteLinkedData;
+import org.schema.CreativeWork;
+import org.stjs.javascript.Array;
+import org.stjs.javascript.Global;
+import org.stjs.javascript.JSCollections;
+import org.stjs.javascript.JSObjectAdapter;
+import org.stjs.javascript.functions.Callback1;
+
 /**
  * Competency is Under construction.
  * 
@@ -7,18 +17,361 @@ package com.eduworks.schema.cfd.competency;
  * 
  * @author debbie.brown@eduworks.com
  * @author devlin.junker@eduworks.com
- * @class Reference
+ * @class CfdReference
  * @module com.eduworks
- * @extends org.cassproject.schema.cass.competency.Competency
+ * @extends org.schema.CreativeWork
  */
-public class CfdReference extends CfdCompetency
-{
-
+public class CfdReference extends CreativeWork{
+	
+	public static final String myType = "http://schema.org/CreativeWork";
+	
+	public static final String edUse = "Reference";
+	
+	public CfdReference(){
+		super();
+		educationalUse=edUse;
+	}
+	
 	/**
-	 * Sub-type of Competency that this is (Will be replaced later)
-	 * @property subtype
-	 * @type string
+	 * Returns the name of the reference
+	 * 
+	 * @return {String} 
+	 * 			name of reference
 	 */
-	public final String subtype = "Reference";
+	public String getName(){
+		return name;
+	}
+	
+	/**
+	 * Sets the Name of the reference
+	 * 
+	 * @param {String} name
+	 * 			name of the reference
+	 */
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	/**
+	 * Returns the URL of the reference
+	 * 
+	 * @return {String} 
+	 * 			URL of reference
+	 */
+	public String getUrl(){
+		return url;
+	}
+	
+	/**
+	 * Sets the URL of the reference
+	 * 
+	 * @param {String} URL
+	 * 			URL the reference is pointing at
+	 */
+	public void setUrl(String url){
+		this.url = url;
+	}
+	
+	/**
+	 * Gets the educational alignment of the reference
+	 * 
+	 * @return {CfdAlignment}
+	 * 			Educational Alignment for reference
+	 */
+	public CfdAlignment getEducationalAlignment(){
+		return (CfdAlignment)educationalAlignment;
+	}
+	
+	/**
+	 * Sets the educational alignment of the reference
+	 * 
+	 * @param {CfdAlignment} alignment
+	 * 			Educational alignment for reference
+	 */
+	public void setEducationalAlignment(CfdAlignment alignment){
+		educationalAlignment = alignment;
+	}
+	
+	/**
+	 * Returns the Educational Use of the reference (Should be Reference)
+	 * 
+	 * @return {String} 
+	 * 			educational use
+	 */
+	public String getEducationalUse(){
+		return educationalUse;
+	}
+	
+	/**
+	 * Saves this alignment details on the server corresponding to its ID 
+	 * 
+	 * @memberOf CfdReference
+	 * @method save
+	 * @param {Callback1<String>} success
+	 * 			Callback triggered on successfully saving the alignment
+	 * @param {Callback1<String>} [failure]
+	 * 			Callback triggered if error while saving alignment
+	 */
+	public void save(Callback1<String> success, Callback1<String> failure){
+		if(name == null || name == ""){
+			String msg = "Source Competency cannot be missing";
+			if(failure != null)
+				failure.$invoke(msg);
+			else
+				Global.console.error(msg);
+			return;
+		}
+		
+		if(url == null || url == ""){
+			String msg = "Relation Type cannot be missing";
+			if(failure != null)
+				failure.$invoke(msg);
+			else
+				Global.console.error(msg);
+			return;
+		}
+		
+		if(educationalAlignment == null){
+			String msg = "Relation Type cannot be missing";
+			if(failure != null)
+				failure.$invoke(msg);
+			else
+				Global.console.error(msg);
+			return;
+		}
+		
+		EcRepository._save(this, success, failure);
+	}
+	
+	
+	/**
+	 * Deletes the alignment from the server corresponding to its ID
+	 * 
+	 * @memberOf CfdReference
+	 * @method _delete
+	 * @param {Callback1<String>} success
+	 * 			Callback triggered on successfully deleting the alignment
+	 * @param {Callback1<String>} [failure]
+	 * 			Callback triggered if error while deleting alignment
+	 */
+	public void _delete(Callback1<String> success, Callback1<String> failure){
+		EcRepository.DELETE(this, success, failure);
+	}
+	
+	/**
+	 * Retrieves the alignment specified with the ID from the server
+	 * 
+	 * @memberOf CfdReference
+	 * @method get
+	 * @static
+	 * @param {String} id
+	 * 			ID of the alignment to retrieve
+	 * @param {Callback1<EcAlignment>} success
+	 * 			Callback triggered on successfully retrieving the alignment,
+	 * 			returns the alignment
+	 * @param {Callback1<String>} [failure]
+	 * 			Callback triggered if error while retrieving alignment
+	 */
+	public static void get(String id, final Callback1<CfdReference> success, final Callback1<String> failure)
+	{
+		EcRepository.get(id, new Callback1<EcRemoteLinkedData>()
+		{
+			@Override
+			public void $invoke(EcRemoteLinkedData p1)
+			{
+                if (p1 instanceof CfdReference)
+                    if (success != null)
+                    {
+                        success.$invoke((CfdReference) p1);
+                        return;
+                    }
+                
+                CfdReference reference = new CfdReference();
+				
+				if (p1.isA(EcEncryptedValue.myType))
+				{
+					EcEncryptedValue encrypted = new EcEncryptedValue();
+					encrypted.copyFrom(p1);
+					p1 = encrypted.decryptIntoObject();
+				}
+				if (p1.isAny(reference.getTypes()))
+				{
+					reference.copyFrom(p1);
+					
+                    if (EcRepository.caching)
+                    {
+                        JSObjectAdapter.$put(EcRepository.cache,reference.shortId(),reference);
+                        JSObjectAdapter.$put(EcRepository.cache,reference.id,reference);
+                    }
+					if(success != null)
+						success.$invoke(reference);
+				}
+				else
+				{
+					String msg = "Resultant object is not a relation.";
+					if (failure != null)
+						failure.$invoke(msg);
+					else
+						Global.console.error(msg);
+				}
+				
+			}
+		}, failure);
+	}
+	
+	/**
+	 * Retrieves an alignment from it's server synchronously, the call 
+	 * blocks until it is successful or an error occurs
+	 * 
+	 * @memberOf CfdReference
+	 * @method getBlocking
+	 * @static
+	 * @param {String} id
+	 * 			ID of the alignment to retrieve
+	 * @return EcAlignment
+	 * 			The alignment retrieved
+	 */
+	public static CfdReference getBlocking(String id)
+	{
+		EcRemoteLinkedData p1 = EcRepository.getBlocking(id);
+                if (p1 instanceof CfdReference)
+                    return (CfdReference) p1;
 
+        CfdReference reference = new CfdReference();
+
+		if (p1.isA(EcEncryptedValue.myType))
+		{
+			EcEncryptedValue encrypted = new EcEncryptedValue();
+			encrypted.copyFrom(p1);
+			p1 = encrypted.decryptIntoObject();
+
+			EcEncryptedValue.encryptOnSave(p1.id, true);
+		}
+		if (p1.isAny(reference.getTypes()))
+		{
+			reference.copyFrom(p1);
+                    if (EcRepository.caching)
+                    {
+                        JSObjectAdapter.$put(EcRepository.cache,reference.shortId(),reference);
+                        JSObjectAdapter.$put(EcRepository.cache,reference.id,reference);
+                    }
+			return reference;
+		} 
+                else
+		{
+			String msg = "Retrieved object was not a relation";
+			Global.console.error(msg);
+			return null;
+		}
+	}
+        
+	/**
+	 * Searches the repository using the query and optional parameters provided
+	 * 
+	 * @memberOf CfdReference
+	 * @method search
+	 * @static
+	 * @param {EcRepository} repo
+	 * 			Repository to search using the query provided
+	 * @param {String} query
+	 * 			The query to send to the search
+	 * @param {Callback1<Array<EcAlignment>>} success
+	 * 			Callback triggered on successful search return
+	 * @param {Callback1<String>} [failure]
+	 * 			Callback triggered if error searching
+	 * @param {Object} [paramObj]
+	 * 			Parameters to include in the search
+	 * 		@param start
+	 * 		@param size
+	 */
+	public static void search(EcRepository repo, String query, final Callback1<Array<CfdReference>> success, Callback1<String> failure, Object paramObj){
+		String queryAdd = new CfdReference().getSearchStringByType();
+		
+		if (query == null || query == "")
+			query = queryAdd;
+		else
+			query = "(" + query + ") AND educationalUse:\""+edUse+"\" AND " + queryAdd;
+		
+		repo.searchWithParams(query, paramObj, null, new Callback1<Array<EcRemoteLinkedData>>(){
+
+			@Override
+			public void $invoke(Array<EcRemoteLinkedData> p1) {
+				if(success != null)
+				{
+					Array<CfdReference> ret = JSCollections.$array();
+					for(int i = 0; i < p1.$length(); i++){
+						CfdReference reference = new CfdReference();
+						if(p1.$get(i).isAny(reference.getTypes())){
+							reference.copyFrom(p1.$get(i));
+						}else if(p1.$get(i).isA(EcEncryptedValue.myType)){
+							EcEncryptedValue val = new EcEncryptedValue();
+							val.copyFrom(p1.$get(i));
+							if(val.isAnEncrypted(CfdReference.myType)){
+								EcRemoteLinkedData obj = val.decryptIntoObject();
+								reference.copyFrom(obj);
+							}
+						}
+						
+						ret.$set(i, reference);
+					}
+					
+					success.$invoke(ret);
+				}
+			}
+			
+		}, failure);
+	}
+	
+	/**
+	 *  Searches the repository for references with the framework and optional parameters provided
+	 * 
+	 * @memberOf CfdReference
+	 * @method search
+	 * @static
+	 * @param {EcRepository} repo
+	 * 			Repository to search using the query provided
+	 * @param {String} Framework
+	 * 			The Framework to search for References with EducationalAlignments too
+	 * @param {Callback1<Array<EcAlignment>>} success
+	 * 			Callback triggered on successful search return
+	 * @param {Callback1<String>} [failure]
+	 * 			Callback triggered if error searching
+	 * @param {Object} [paramObj]
+	 * 			Parameters to include in the search
+	 * 		@param start
+	 * 		@param size
+	 */
+	public static void searchWithFramework(EcRepository repo, String framework, final Callback1<Array<CfdReference>> success, Callback1<String> failure, Object paramObj){
+		String query = new CfdReference().getSearchStringByType();
+		query = "(" + query + ") AND educationalUse:\""+edUse+"\" AND educationalAlignment.educationalFramework:\""+framework+"\"";
+		
+		repo.searchWithParams(query, paramObj, null, new Callback1<Array<EcRemoteLinkedData>>(){
+
+			@Override
+			public void $invoke(Array<EcRemoteLinkedData> p1) {
+				if(success != null)
+				{
+					Array<CfdReference> ret = JSCollections.$array();
+					for(int i = 0; i < p1.$length(); i++){
+						CfdReference reference = new CfdReference();
+						if(p1.$get(i).isAny(reference.getTypes())){
+							reference.copyFrom(p1.$get(i));
+						}else if(p1.$get(i).isA(EcEncryptedValue.myType)){
+							EcEncryptedValue val = new EcEncryptedValue();
+							val.copyFrom(p1.$get(i));
+							if(val.isAnEncrypted(CfdReference.myType)){
+								EcRemoteLinkedData obj = val.decryptIntoObject();
+								reference.copyFrom(obj);
+							}
+						}
+						
+						ret.$set(i, reference);
+					}
+					
+					success.$invoke(ret);
+				}
+			}
+			
+		}, failure);
+	}
 }
