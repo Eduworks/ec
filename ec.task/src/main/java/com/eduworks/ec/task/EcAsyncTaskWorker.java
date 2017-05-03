33ec661adc42ffe1e56eee5e333bc43136b3dd56
devlin junker <devlin.junker@eduworks.com>
2017-05-03 10:59:40 -0700

    Started ec.task
    Added EcAsyncTask, EcAsyncTaskWorker, and EcAsyncTaskManager

---
+++ ec.task/src/main/java/com/eduworks/ec/task/EcAsyncTaskWorker.java
@@ -0,0 +1,80 @@
+package com.eduworks.ec.task;
+
+import org.stjs.javascript.Array;
+import org.stjs.javascript.Global;
+import org.stjs.javascript.JSCollections;
+import org.stjs.javascript.JSGlobal;
+import org.stjs.javascript.TimeoutHandler;
+import org.stjs.javascript.functions.Callback0;
+
+public class EcAsyncTaskWorker{
+	
+	EcAsyncTask currentTask;
+	Array<EcAsyncTask> assignedList;
+	
+	public int checkWorkInterval = 100;
+	
+	public TimeoutHandler intervalHandler;
+	public TimeoutHandler taskHandler;
+	
+	public EcAsyncTaskWorker(){
+		assignedList = JSCollections.$array();
+		final EcAsyncTaskWorker that = this;
+		intervalHandler = Global.setInterval(new Callback0() {
+			@Override
+			public void $invoke() {
+				that.run();
+			}
+		}, checkWorkInterval);
+	}
+
+	public int getAssignedCount(){
+		return assignedList.$length();
+	}
+	
+	public void assign(EcAsyncTask task){
+		if(currentTask == null || currentTask == JSGlobal.undefined || currentTask.getIsComplete()){
+			currentTask = task;
+		}else{
+			assignedList.push(task);
+		}
+	}
+	
+	private void invoke(){
+		if(!currentTask.getIsComplete()){
+			Array<Object> args = currentTask.getArguments();
+	 	
+			if(args == null || args == JSGlobal.undefined || args.$length() == 0){
+				currentTask.doTask();
+			}else if(args.$length() == 1){
+				currentTask.doTask(args.$get(0));
+			}else if(args.$length() == 2){
+				currentTask.doTask(args.$get(0), args.$get(1));
+			}else if(args.$length() == 3){
+				currentTask.doTask(args.$get(0), args.$get(1), args.$get(2));
+			}else if(args.$length() == 4){
+				currentTask.doTask(args.$get(0), args.$get(1), args.$get(2), args.$get(3));
+			}
+		}
+	}
+	
+	protected void run() {
+		final EcAsyncTaskWorker that = this;
+		taskHandler = Global.setTimeout(new Callback0(){
+
+			@Override
+			public void $invoke() {			 
+				if(that.currentTask != JSGlobal.undefined && that.currentTask != null && !that.currentTask.getIsStarted()){
+					that.invoke();
+				}
+				
+				if(that.currentTask != JSGlobal.undefined && that.currentTask != null && that.currentTask.getIsComplete()){
+					that.currentTask = that.assignedList.shift();
+				}
+				
+			}
+		}, 25);
+		
+	}
+
+}
