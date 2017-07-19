add075486b074f4b88eaa8c166e7ff8a22d26577
winonadeb <winonadeb@gmail.com>
2017-07-19 10:16:39 -0500

    Just adding ANGLES Question object

---
+++ org.angles.schema.angles/src/main/java/org/angles/schema/angles/Question.java
@@ -0,0 +1,21 @@
+package org.angles.schema.angles;
+
+import org.schema.Question;
+
+/**
+ * Created by dbrown on 5/18/17.
+ */
+public class Question extends Question {
+
+	/**
+	 * Constructor, automatically sets @context and @type.
+	 * @constructor
+	 */
+	public Question()
+	{
+		context="http://schema.eduworks.com/angles/0.1/";
+		type="Question";
+	}
+
+
+}
