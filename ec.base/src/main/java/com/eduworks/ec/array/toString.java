decee1c8cb8af6756e2806bf17feed6f2dec5cc8
Ronald Ray <fritz.ray@eduworks.com>
2017-11-27 16:22:23 -0800

    Fully qualified toString for IE 11.

--- ec.base/src/main/java/com/eduworks/ec/array/toString.java
+++ ec.base/src/main/java/com/eduworks/ec/array/toString.java
@@ -1,8 +1,10 @@
 package com.eduworks.ec.array;
 
+import org.stjs.javascript.annotation.Namespace;
 import org.stjs.javascript.annotation.STJSBridge;
 
 @STJSBridge()
+@Namespace("Object.prototype")
 public class toString {
 	public static String call(Object o) {
 		return null;
