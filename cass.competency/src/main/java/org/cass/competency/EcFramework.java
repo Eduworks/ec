40b957a0c7e5902991743de0bca3ec76d13fd2d7
Ronald Ray <fritz.ray@eduworks.com>
2018-04-13 16:54:11 -0700

    Fixed double success invoke bug on removing competencies.

--- cass.competency/src/main/java/org/cass/competency/EcFramework.java
+++ cass.competency/src/main/java/org/cass/competency/EcFramework.java
@@ -219,8 +219,10 @@ public class EcFramework extends Framework {
 			if (competency.$get(i).equals(shortId) || competency.$get(i).equals(id))
 				competency.splice(i, 1);
 		if ((relation == null || relation.$length() == 0) && (level == null || level.$length() == 0))
-			if (success != null)
+			if (success != null) {
 				success.$invoke("");
+				return;
+			}
 
 		relDone.$put(id, false);
 		levelDone.$put(id, false);
@@ -278,25 +280,21 @@ public class EcFramework extends Framework {
 		if (i >= relation.$length() && success != null)
 			success.$invoke("");
 		else
-			EcRepository.get(relation.$get(i), new Callback1<EcRemoteLinkedData>() {
+			EcAlignment.get(relation.$get(i), new Callback1<EcAlignment>() {
 				@Override
-				public void $invoke(EcRemoteLinkedData p1) {
-					EcAlignment a = null;
-
-					// Wrap this in case there's an error retrieving, will skip that relationship
-					try {
-						a = new EcAlignment();
-						a.copyFrom(p1);
-					} catch (Exception e) {
-					}
-
+				public void $invoke(EcAlignment a) {
 					if (a != null && a.source == shortId || a.target == shortId || a.source == id || a.target == id) {
 						me.relation.splice(i, 1);
 						me.removeRelationshipsThatInclude(id, i, success, failure);
 					} else
 						me.removeRelationshipsThatInclude(id, i + 1, success, failure);
 				}
-			}, failure);
+			}, new Callback1<String>() {
+				@Override
+				public void $invoke(String s) {
+					me.removeRelationshipsThatInclude(id, i + 1, success, failure);
+				}
+			});
 	}
 
 	/**
@@ -320,18 +318,21 @@ public class EcFramework extends Framework {
 		if (i >= level.$length() && success != null)
 			success.$invoke("");
 		else
-			EcRepository.get(level.$get(i), new Callback1<EcRemoteLinkedData>() {
+			EcLevel.get(level.$get(i), new Callback1<EcLevel>() {
 				@Override
-				public void $invoke(EcRemoteLinkedData p1) {
-					EcLevel a = new EcLevel();
-					a.copyFrom(p1);
+				public void $invoke(EcLevel a) {
 					if (a.competency == shortId || a.competency == id) {
 						me.level.splice(i, 1);
 						me.removeLevelsThatInclude(id, i, success, failure);
 					} else
 						me.removeLevelsThatInclude(id, i + 1, success, failure);
 				}
-			}, failure);
+			}, new Callback1<String>() {
+				@Override
+				public void $invoke(String s) {
+					me.removeLevelsThatInclude(id, i + 1, success, failure);
+				}
+			});
 	}
 
 	/**
