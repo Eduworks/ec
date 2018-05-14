24e2dc212be89c63e0cfab774c6ee487bbd00ef2
Ronald Ray <fritz.ray@eduworks.com>
2018-05-14 07:42:28 -0700

    BrowserDetection no longer throws exceptions in Node.

--- ec.base/src/main/java/com/eduworks/ec/browser/EcBrowserDetection.java
+++ ec.base/src/main/java/com/eduworks/ec/browser/EcBrowserDetection.java
@@ -3,8 +3,12 @@ package com.eduworks.ec.browser;
 import org.stjs.javascript.Global;
 
 public class EcBrowserDetection {
-	public static Boolean isIeOrEdge() {
-		return Global.window.navigator.appName == "Microsoft Internet Explorer" ||
-				(Global.window.navigator.appName == "Netscape" && Global.window.navigator.appVersion.indexOf("Edge") > -1);
-	}
+    public static Boolean isIeOrEdge() {
+        if (Global.window == null) return false;
+        if (Global.window.navigator == null) return false;
+        if (Global.window.navigator.appName == null) return false;
+
+        return Global.window.navigator.appName == "Microsoft Internet Explorer" ||
+                (Global.window.navigator.appName == "Netscape" && Global.window.navigator.appVersion.indexOf("Edge") > -1);
+    }
 }
