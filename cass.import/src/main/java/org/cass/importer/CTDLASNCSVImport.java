4a141610644ab5488db0bec01119a85254fdd510
Ronald Ray <fritz.ray@eduworks.com>
2019-04-16 11:05:49 -0700

    Started work to use translation contexts to parse CSVs.

--- cass.import/src/main/java/org/cass/importer/CTDLASNCSVImport.java
+++ cass.import/src/main/java/org/cass/importer/CTDLASNCSVImport.java
@@ -1,5 +1,6 @@
 package org.cass.importer;
 
+import com.eduworks.ec.array.EcAsyncHelper;
 import com.eduworks.ec.crypto.EcPpk;
 import js.Papa;
 import js.PapaParseParams;
@@ -10,8 +11,10 @@ import org.cassproject.ebac.identity.EcIdentity;
 import org.cassproject.ebac.identity.EcIdentityManager;
 import org.cassproject.ebac.repository.EcRepository;
 import org.cassproject.schema.cass.competency.Relation;
+import org.json.ld.EcLinkedData;
 import org.stjs.javascript.Array;
 import org.stjs.javascript.JSObjectAdapter;
+import org.stjs.javascript.functions.Callback0;
 import org.stjs.javascript.functions.Callback1;
 import org.stjs.javascript.functions.Callback2;
 import org.stjs.javascript.functions.Callback3;
@@ -94,137 +97,137 @@ public class CTDLASNCSVImport {
 				complete = new Callback1<Object>() {
 					@Override
 					public void $invoke(Object results) {
-						Array<Array<String>> tabularData = (Array<Array<String>>) JSObjectAdapter.$get(results, "data");
+						Array<Object> tabularData = (Array<Object>) JSObjectAdapter.$get(results, "data");
 
-						Object frameworks = new Object();
-						Array frameworkArray = new Array<EcFramework>();
-						Object frameworkRows = new Object();
-						Array competencies = new Array<EcCompetency>();
-						Object competencyRows = new Object();
-						Array relations = new Array<EcAlignment>();
-						Object relationById = new Object();
-						for (int i = 0; i < tabularData.$length(); i++) {
-							Object e = tabularData.$get(i);
-							if (JSObjectAdapter.$get(e, "@type") == "ceasn:CompetencyFramework") {
-								EcFramework f = new EcFramework();
+						final Object frameworks = new Object();
+						final Array frameworkArray = new Array<EcFramework>();
+						final Object frameworkRows = new Object();
+						final Array competencies = new Array<EcCompetency>();
+						final Object competencyRows = new Object();
+						final Array relations = new Array<EcAlignment>();
+						final Object relationById = new Object();
+						new EcAsyncHelper<Object>().each(tabularData, new Callback2<Object, Callback0>() {
+							@Override
+							public void $invoke(Object pretranslatedE, final Callback0 callback0) {
+								if (JSObjectAdapter.$get(pretranslatedE, "@type") == "ceasn:CompetencyFramework") {
+									EcLinkedData translator = new EcLinkedData(null, null);
+									translator.copyFrom(pretranslatedE);
+									translator.recast("http://schema.cassproject.org/0.3/ceasn2cass", "http://schema.cassproject.org/0.3", new Callback1<EcLinkedData>() {
+										@Override
+										public void $invoke(EcLinkedData e) {
+											EcFramework f = new EcFramework();
+											f.copyFrom(e);
+											if (JSObjectAdapter.$get(e, "@owner") != null) {
+												EcIdentity id = new EcIdentity();
+												id.ppk = EcPpk.fromPem((String) JSObjectAdapter.$get(e, "@owner"));
+												if (ceo != null)
+													f.addOwner(ceo.ppk.toPk());
+												f.addOwner(id.ppk.toPk());
+												EcIdentityManager.addIdentityQuietly(id);
+											}
 
-								if (JSObjectAdapter.$get(e, "@owner") != null) {
-									EcIdentity id = new EcIdentity();
-									id.ppk = EcPpk.fromPem((String) JSObjectAdapter.$get(e, "@owner"));
-									if (ceo != null)
-										f.addOwner(ceo.ppk.toPk());
-									f.addOwner(id.ppk.toPk());
-									EcIdentityManager.addIdentityQuietly(id);
-								}
-
-								f.id = (String) JSObjectAdapter.$get(e, "@id");
-								JSObjectAdapter.$put(frameworks, f.id, f);
-								JSObjectAdapter.$put(frameworkRows, f.id, e);
-								frameworkArray.push(f);
-								f.competency = new Array();
-								f.relation = new Array();
-								f.name = (String) JSObjectAdapter.$get(e, "ceasn:name");
-								if (JSObjectAdapter.$get(e, "ceasn:creator") != null)
-									JSObjectAdapter.$put(e, "ceasn:creator", ((String) JSObjectAdapter.$get(e, "ceasn:creator")).toLowerCase());
-								JSObjectAdapter.$put(f, "schema:creator", JSObjectAdapter.$get(e, "ceasn:creator"));
-								JSObjectAdapter.$put(f, "ceasn:derivedFrom", JSObjectAdapter.$get(e, "ceasn:derivedFrom"));
-								JSObjectAdapter.$put(f, "dc:source", JSObjectAdapter.$get(e, "ceasn:source"));
-							} else if (JSObjectAdapter.$get(e, "@type") == "ceasn:Competency") {
-								EcCompetency f = new EcCompetency();
-								if (JSObjectAdapter.$get(e, "@id") == null) continue;
-								f.id = (String) JSObjectAdapter.$get(e, "@id");
-								if (JSObjectAdapter.$get(e, "ceasn:isPartOf") != null) {
-									((EcFramework) JSObjectAdapter.$get(frameworks, (String) JSObjectAdapter.$get(e, "ceasn:isPartOf"))).competency.push(f.shortId());
-								} else {
-									Object parent = e;
-									boolean done = false;
-									while (!done && parent != null) {
-										if (JSObjectAdapter.$get(parent, "ceasn:isChildOf") != null && JSObjectAdapter.$get(parent, "ceasn:isChildOf") != "") {
-											parent = JSObjectAdapter.$get(competencyRows, (String) JSObjectAdapter.$get(parent, "ceasn:isChildOf"));
-										} else if (JSObjectAdapter.$get(parent, "ceasn:isTopChildOf") != null && JSObjectAdapter.$get(parent, "ceasn:isTopChildOf") != "") {
-											parent = JSObjectAdapter.$get(frameworkRows, (String) JSObjectAdapter.$get(parent, "ceasn:isTopChildOf"));
-											done = true;
+											JSObjectAdapter.$put(frameworks, f.id, f);
+											JSObjectAdapter.$put(frameworkRows, f.id, e);
+											frameworkArray.push(f);
+											f.competency = new Array();
+											f.relation = new Array();
+											//Delete ceasn:hasTopChild, ceasn:isChildOf, ceasn:hasChild, etc.
+											callback0.$invoke();
 										}
-									}
-									if (!done) {
-										error.$invoke("Could not find framework:" + JSObjectAdapter.$get(e, "@type"));
-										return;
-									}
-									if (parent != null) {
-										if (JSObjectAdapter.$get(parent, "@type") == "ceasn:CompetencyFramework") {
-											JSObjectAdapter.$put(e, "ceasn:isPartOf", (String) JSObjectAdapter.$get(parent, "@id"));
-											((EcFramework) JSObjectAdapter.$get(frameworks, (String) JSObjectAdapter.$get(parent, "@id"))).competency.push(f.shortId());
-										} else {
-											error.$invoke("Object cannot trace to framework:" + JSObjectAdapter.$get(e, "@type"));
-											return;
+									}, (Callback1) failure);
+								} else if (JSObjectAdapter.$get(pretranslatedE, "@type") == "ceasn:Competency") {
+									EcLinkedData translator = new EcLinkedData(null, null);
+									translator.copyFrom(pretranslatedE);
+									translator.recast("http://schema.cassproject.org/0.3/ceasn2cass", "http://schema.cassproject.org/0.3", new Callback1<EcLinkedData>() {
+										@Override
+										public void $invoke(EcLinkedData e) {
+											EcCompetency f = new EcCompetency();
+											f.copyFrom(e);
+											if (JSObjectAdapter.$get(e, "@id") == null) {
+												callback0.$invoke();
+												return;
+											}
+											if (JSObjectAdapter.$get(e, "ceasn:isPartOf") != null) {
+												((EcFramework) JSObjectAdapter.$get(frameworks, (String) JSObjectAdapter.$get(e, "ceasn:isPartOf"))).competency.push(f.shortId());
+											} else {
+												Object parent = e;
+												boolean done = false;
+												while (!done && parent != null) {
+													if (JSObjectAdapter.$get(parent, "ceasn:isChildOf") != null && JSObjectAdapter.$get(parent, "ceasn:isChildOf") != "") {
+														parent = JSObjectAdapter.$get(competencyRows, (String) JSObjectAdapter.$get(parent, "ceasn:isChildOf"));
+													} else if (JSObjectAdapter.$get(parent, "ceasn:isTopChildOf") != null && JSObjectAdapter.$get(parent, "ceasn:isTopChildOf") != "") {
+														parent = JSObjectAdapter.$get(frameworkRows, (String) JSObjectAdapter.$get(parent, "ceasn:isTopChildOf"));
+														done = true;
+													}
+												}
+												if (!done) {
+													error.$invoke("Could not find framework:" + JSObjectAdapter.$get(e, "@type"));
+													return;
+												}
+												if (parent != null) {
+													if (JSObjectAdapter.$get(parent, "@type") == "ceasn:CompetencyFramework") {
+														JSObjectAdapter.$put(e, "ceasn:isPartOf", (String) JSObjectAdapter.$get(parent, "@id"));
+														((EcFramework) JSObjectAdapter.$get(frameworks, (String) JSObjectAdapter.$get(parent, "@id"))).competency.push(f.shortId());
+													} else {
+														error.$invoke("Object cannot trace to framework:" + JSObjectAdapter.$get(e, "@type"));
+														return;
+													}
+												} else {
+													error.$invoke("Object has no framework:" + JSObjectAdapter.$get(e, "@type"));
+													return;
+												}
+											}
+
+											if (JSObjectAdapter.$get(e, "@owner") == null) {
+												if (JSObjectAdapter.$get(JSObjectAdapter.$get(frameworkRows, (String) JSObjectAdapter.$get(e, "ceasn:isPartOf")), "@owner") != null)
+													JSObjectAdapter.$put(e, "@owner", JSObjectAdapter.$get(JSObjectAdapter.$get(frameworkRows, (String) JSObjectAdapter.$get(e, "ceasn:isPartOf")), "@owner"));
+											}
+											EcIdentity id = new EcIdentity();
+											if (JSObjectAdapter.$get(e, "@owner") != null) {
+												id.ppk = EcPpk.fromPem((String) JSObjectAdapter.$get(e, "@owner"));
+												if (ceo != null)
+													f.addOwner(ceo.ppk.toPk());
+												if (id.ppk != null)
+													f.addOwner(id.ppk.toPk());
+												EcIdentityManager.addIdentityQuietly(id);
+											}
+
+											if (JSObjectAdapter.$get(e, "ceasn:isChildOf") != null) {
+												EcAlignment r = new EcAlignment();
+												r.generateId(repo.selectedServer);
+												if (ceo != null)
+													r.addOwner(ceo.ppk.toPk());
+												if (id.ppk != null)
+													r.addOwner(id.ppk.toPk());
+												r.source = (String) JSObjectAdapter.$get(e, "@id");
+												r.relationType = Relation.NARROWS;
+												r.target = (String) JSObjectAdapter.$get(e, "ceasn:isChildOf");
+												relations.push(r);
+												JSObjectAdapter.$put(relationById, r.shortId(), r);
+												((EcFramework) JSObjectAdapter.$get(frameworks, (String) JSObjectAdapter.$get(e, "ceasn:isPartOf"))).relation.push(r.shortId());
+											}
+											//TODO: Handle other types of alignments other than broadens/narrows/haschild/ischild
+											competencies.push(f);
+											JSObjectAdapter.$put(competencyRows, f.id, e);
+											//Delete ceasn:hasTopChild, ceasn:isChildOf, ceasn:hasChild, etc.
+											callback0.$invoke();
 										}
-									} else {
-										error.$invoke("Object has no framework:" + JSObjectAdapter.$get(e, "@type"));
-										return;
-									}
-								}
+									}, (Callback1) failure);
+								} else if (JSObjectAdapter.$get(pretranslatedE, "@type") == null || JSObjectAdapter.$get(pretranslatedE, "@type") == "")
+								{callback0.$invoke();return;}
 
-								if (JSObjectAdapter.$get(e, "@owner") == null) {
-									if (JSObjectAdapter.$get(JSObjectAdapter.$get(frameworkRows, (String) JSObjectAdapter.$get(e, "ceasn:isPartOf")), "@owner") != null)
-										JSObjectAdapter.$put(e, "@owner", JSObjectAdapter.$get(JSObjectAdapter.$get(frameworkRows, (String) JSObjectAdapter.$get(e, "ceasn:isPartOf")), "@owner"));
+								else {
+									error.$invoke("Found unknown type:" + JSObjectAdapter.$get(pretranslatedE, "@type"));
+									callback0.$invoke();
+									return;
 								}
-								EcIdentity id = new EcIdentity();
-								if (JSObjectAdapter.$get(e, "@owner") != null) {
-									id.ppk = EcPpk.fromPem((String) JSObjectAdapter.$get(e, "@owner"));
-									if (ceo != null)
-										f.addOwner(ceo.ppk.toPk());
-									if (id.ppk != null)
-										f.addOwner(id.ppk.toPk());
-									EcIdentityManager.addIdentityQuietly(id);
-								}
-
-								f.name = (String) JSObjectAdapter.$get(e, "ceasn:competencyText");
-								if (f.name == null || f.name == "")
-									f.name = (String) JSObjectAdapter.$get(e, "ceasn:name");
-								if (JSObjectAdapter.$get(e, "ceasn:comment") != null)
-									f.description = (String) JSObjectAdapter.$get(e, "ceasn:comment");
-								JSObjectAdapter.$put(f, "schema:creator", JSObjectAdapter.$get(e, "ceasn:creator"));
-								JSObjectAdapter.$put(f, "ceasn:codedNotation", JSObjectAdapter.$get(e, "ceasn:codedNotation"));
-								JSObjectAdapter.$put(f, "ceasn:listID", JSObjectAdapter.$get(e, "ceasn:listID"));
-
-								if (JSObjectAdapter.$get(e, "ceasn:isChildOf") != null) {
-									EcAlignment r = new EcAlignment();
-									r.generateId(repo.selectedServer);
-									if (ceo != null)
-										r.addOwner(ceo.ppk.toPk());
-									if (id.ppk != null)
-										r.addOwner(id.ppk.toPk());
-									r.source = (String) JSObjectAdapter.$get(e, "@id");
-									r.relationType = Relation.NARROWS;
-									r.target = (String) JSObjectAdapter.$get(e, "ceasn:isChildOf");
-									relations.push(r);
-									JSObjectAdapter.$put(relationById, r.shortId(), r);
-									((EcFramework) JSObjectAdapter.$get(frameworks, (String) JSObjectAdapter.$get(e, "ceasn:isPartOf"))).relation.push(r.shortId());
-								}
-//								if (e["schema:sameAs"] != null)
-//									for (var sameAs in e["schema:sameAs"].split(" ")) {
-//									EcAlignment r = new EcAlignment();
-//									r.generateId(repo.selectedServer);
-//									r.addOwner(ceo.ppk.toPk());
-//									r.addOwner(id.ppk.toPk());
-//									r.source = e["@id"];
-//									r.relationType = Relation.IS_EQUIVALENT_TO;
-//									r.target = sameAs;
-//									relations.push(r);
-//									relationById[r.shortId()] = r;
-//									frameworks[e["ceasn:isPartOf"]].relation.push(r.shortId());
-//								}
-								JSObjectAdapter.$put(f, "ceasn:derivedFrom", JSObjectAdapter.$get(e, "ceasn:derivedFrom"));
-								competencies.push(f);
-								JSObjectAdapter.$put(competencyRows, f.id, e);
-							} else if (JSObjectAdapter.$get(e, "@type") == null || JSObjectAdapter.$get(e, "@type") == "")
-								continue;
-							else {
-								error.$invoke("Found unknown type:" + JSObjectAdapter.$get(e, "@type"));
-								return;
 							}
-						}
-						success.$invoke(frameworkArray, competencies, relations);
+						}, new Callback1<Array<Object>>() {
+							@Override
+							public void $invoke(Array<Object> strings) {
+								success.$invoke(frameworkArray, competencies, relations);
+							}
+						});
 					}
 				};
 				error = failure;
